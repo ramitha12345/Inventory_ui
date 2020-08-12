4e2941854ebbf1ea58897dbf32087ea9ed85835d
Pathum Kalhan <pathumsimpson@gmail.com>
2020-08-12 18:39:39 +0530

    bearer token

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import axios from 'axios';
-import VuexPersistence from 'vuex-persist';
-import Cookies from 'js-cookie';
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+import VuexPersistence from "vuex-persist";
+import Cookies from "js-cookie";
 Vue.use(Vuex);
 const vuexLocal = new VuexPersistence({
   storage: window.sessionStorage,
@@ -12,7 +12,7 @@ export default new Vuex.Store({
   plugins: [vuexLocal.plugin],
   state: {
     token: null,
-    role: ""
+    role: "",
   },
   mutations: {
     setLogin(state, payload) {
@@ -21,37 +21,34 @@ export default new Vuex.Store({
     },
     setLogout(state) {
       state.token = null;
-      state.role = '';
-    }
+      state.role = "";
+    },
   },
   actions: {
-    login({
-      commit,
-    }, payload) {
+    login({ commit }, payload) {
       return new Promise((resolve, reject) => {
         try {
-          axios.defaults.headers.common.Authorization = payload.token;
-          Cookies.set('token', payload.token);
-          commit('setLogin', payload);
-          resolve('done');
+          axios.defaults.headers.common.Authorization =
+            "Bearer" + " " + payload.token;
+          Cookies.set("token", payload.token);
+          commit("setLogin", payload);
+          resolve("done");
         } catch (error) {
           reject(error);
         }
       });
     },
-    onLogout({
-      commit,
-    }) {
+    onLogout({ commit }) {
       return new Promise((resolve, reject) => {
         try {
           axios.defaults.headers.common.Authorization = null;
-          Cookies.remove('token');
-          commit('setLogout');
-          resolve('done');
+          Cookies.remove("token");
+          commit("setLogout");
+          resolve("done");
         } catch (error) {
           reject(error);
         }
-      })
+      });
     },
   },
   modules: {},
